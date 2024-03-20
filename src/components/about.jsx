e1ae4ee65b7efcd2c62456ee3507b9a09604ed8e
Kevin Irungu <kt@Kevins-MacBook-Air.local>
2024-03-20 10:36:46 +0300

    Coded About Page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import "./about.css"
+import idea from '../idea.png'
+// import development from '../development.png'
+// import design from '../design.png'
+// import success from '../success.png'
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about_container'>
+      {/* Section with text */}
+      <div className="about_text">
+        <h1 className='about_title'>We design digital products</h1>
+        <div className="line">---------------</div>
+        <p className='about_subtitle'>Your brand, your productYour brand, your productYour brand, your productYour brand, your product, your big idea. We believe in creating opportunities for elite brands, intrepid startups and passionate innovators to change the world.</p>
+      </div>
+
+      {/* Section with visuals */}
+      <div className="about_images">
+        <div className="visual">
+          <div className="visual_icon">
+          <img src={idea} alt='icon' className='icon'></img>
+          </div>
+          <div className="visual_text">
+            IDEA
+          </div>
+        </div>
+
+        <div className="visual">
+          <div className="visual_icon">
+          <img src={idea} alt='icon' className='icon'></img>
+          </div>
+          <div className="visual_text">
+            DESIGN
+          </div>
+        </div>
+
+        <div className="visual">
+          <div className="visual_icon">
+          <img src={idea} alt='icon' className='icon'></img>
+
+          </div>
+          <div className="visual_text">
+            DEVELOPMENT
+          </div>
+        </div>
+
+        <div className="visual">
+          <div className="visual_icon">
+            <img src={idea} alt='icon' className='icon'></img>
+          </div>
+          <div className="visual_text">
+            SUCCESS
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
 
       {/* Section containing text */}
       <div className='home_text'>
-        <h1>Manage inventory</h1>
-        <p>Simplify your stock control, reduce errors, and save time with our intelligent inventory system.</p>
+        <h1 className='title'>Manage inventory</h1>
+        <p className='subtitle'>Simplify your stock control, reduce errors, and save time with our intelligent inventory system.</p>
         <a href='signup' className='getstarted'>Get Started Now </a>
       </div>
 
